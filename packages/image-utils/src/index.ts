72fc48ba47cdb438b66512f5623b2999c724bd8f
Paul Le Cam <paul@3box.io>
2022-02-07 11:34:31 +0000

    Fix typos in docs

--- packages/image-utils/src/index.ts
+++ packages/image-utils/src/index.ts
@@ -4,8 +4,8 @@
  * ## Purpose
  *
  * The `image-utils` module provides various utility functions for images, such as selecting the
- * most suitable format among sources, resizing and uploading, mostly meant to be used in a Web
- * browser context.
+ * most suitable format among sources, resizing and uploading, and is mostly meant to be used in a
+ * Web browser context.
  *
  * ## Installation
  *
